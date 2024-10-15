3356abf63c78bf742b71a8209db79080d118d635
Juan Revilla Atienza <jrevillatienza@hotmail.com>
2024-10-15 16:13:47 +0200

    first commit

---
+++ WPF_CLiente/Cliente.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Controls;
+
+namespace WPF_CLiente
+{
+    public class Cliente
+    {
+        private Socket clienteTCP = null;
+        private IPEndPoint ipEP = null;
+        private bool online = false;        
+    public Cliente(String dirIP, Int32 puerto) 
+        {
+            try
+            {
+                IPAddress dir = IPAddress.Parse(dirIP);
+                ipEP = new IPEndPoint(dir, puerto);
+                clienteTCP = new Socket(dir.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                clienteTCP.SendTimeout = 1000;
+                clienteTCP.ReceiveTimeout = 1000;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al inicial la conexion\n" + ex.ToString(), "Error cliente TCP");
+                throw;
+
+            }
+        }
+        public bool conectarAlServidor()
+        {
+            try
+            {
+                clienteTCP.Connect(ipEP);
+            }
+            catch (SocketException ex_sock)
+            {
+                if(ex_sock.SocketErrorCode == SocketError.ConnectionRefused)
+                {
+                MessageBox.Show("ERROr con la ip o el puerto",
+                    "Error cliente TCP"+ ipEP.Address.ToString()+ "con puerto" + ipEP.Port.ToString());
+
+                }
+                else
+                {
+
+                    MessageBox.Show("Error al conectarse al servidor\n" + ex_sock.ToString(),
+                        "Error cliente TCP" );
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectarse al servidor\n" + ex.ToString(), "Error cliente TCP");
+                throw;
+            }
+            finally
+            {
+                if (clienteTCP != null && clienteTCP.Connected) online = true;
+                else online = false;
+            }
+            return (online);
+        }
+        public void cierraCliente()
+        {
+            if(clienteTCP != null && online)
+            {
+                clienteTCP.Close();
+            }
+            clienteTCP = null;
+            online=false;
+            return;
+        }
+        public int enviaDatos(byte[] datos, int dim)
+        { try
+            {
+                if (online)
+                {
+                    int res = clienteTCP.Send(datos, dim, SocketFlags.None);
+                    if (res == dim)
+                        return (res);
+                    else if (res == 0)
+                    {
+                        clienteTCP = null;
+                        online = false;
+                        return (-1);
+                    }
+                    else
+                    {
+                        return (-2);
+                    }
+                }
+                else
+                    return (-1);
+            }
+            catch (SocketException ex_sock) {
+                if (ex_sock.SocketErrorCode == SocketError.TimedOut)
+                    return (0);
+                else if (ex_sock.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    clienteTCP = null;
+                    online = false;
+                    return (-1);
+                }
+                else
+                    return (-2);
+            }
+            catch(Exception ex) { return (-2); }
+
+                
+        }
+        public int recibeDatos(byte[] datos, int dimMAX)
+        {
+            try
+            {
+                if (online)
+                {
+                    int res = clienteTCP.Receive(datos, dimMAX, SocketFlags.None);
+                    if (res > 0)
+                        return (res);
+                    else if (res == 0)
+                    {
+                        clienteTCP = null;
+                        online = false;
+                        return (-1);
+                    }
+                    else
+                    {
+                        return (-2);
+                    }
+                }
+                else
+                    return (-1);
+            }
+            catch (SocketException ex_sock)
+            {
+                if (ex_sock.SocketErrorCode == SocketError.TimedOut)
+                    return (0);
+                else if (ex_sock.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    clienteTCP = null;
+                    online = false;
+                    return (-1);
+                }
+                else
+                    return (-2);
+            }
+            catch (Exception ex) { return (-2); }     
+          }
+
+    }
+}
---
+++ WPF_CLiente/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPF_CLiente
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Campos:
+        private Cliente clie = null;
+        private bool online = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Boton_salir(object sender, RoutedEventArgs e)
+        {
+            if (clie != null) clie.cierraCliente();
+            clie = null;
+            Close();
+
+        }
+        private void Boton_connectar(object sender, RoutedEventArgs e)
+        {
+            if(!online)
+            {
+                clie= new Cliente(tb_DirrecionIP.Text, Convert.ToInt32(tb_Puerto.Text));
+                if(clie.conectarAlServidor())
+                {
+                    btn_Conectar.Content = "Desconectar";
+                    btn_Enviar.IsEnabled = true;
+                    online = true;
+                }
+                else
+                    clie=null;
+            }
+            else
+            {
+                if (clie != null) clie.cierraCliente();
+                clie = null;
+                btn_Conectar.Content = "Conectar";
+                btn_Enviar.IsEnabled = false;
+                online = false;
+            }
+            return;
+        }
+        private void Boton_enviar(object sender, RoutedEventArgs e)
+        {
+            byte[] estado;
+            byte[] leng = new byte[100];
+            int res;
+            tb_Respuesta.Text = "";
+            if(clie != null && online)
+            {
+                estado= Encoding.UTF8.GetBytes(tb_MensajeEnviar.Text);
+                res = clie.enviaDatos(estado, estado.Length);
+                if (res >= 0) res = clie.recibeDatos(leng, leng.Length);
+                if (res > 0)
+                {
+                    tb_Respuesta.Clear();
+                    for (int i = 0; i < res; i++)
+                        tb_Respuesta.AppendText(leng[i].ToString("X2") + "h ");
+                    tb_MensajeEnviar.Text = "";
+
+                }
+                else if (res == 0)
+                    ;
+                else if (res == -1)
+                {
+                    MessageBox.Show("Se ha cerrado el servidor", "NOtificacion al cliente");
+                    clie.cierraCliente();
+                    clie = null;
+                    btn_Conectar.Content = "Conectar";
+                    btn_Enviar.IsEnabled= false;
+                    online = false;
+
+                }
+                else
+                    MessageBox.Show("Error Desconocido", "Error Cliente TCP");
+            }
+            else
+                MessageBox.Show("No hay conexion con el cliente", "Error Cliente TCP");
+            return;
+        }
+
+        private void tb_Respuesta_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
